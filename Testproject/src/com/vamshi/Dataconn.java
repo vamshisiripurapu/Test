b3b63a857a0562d56405d681a860e27a48d7ee58
svams <svams@vamshikrishna>
2021-08-21 07:48:54 +0530

    Try the test

---
+++ Testproject/src/com/vamshi/Dataconn.java
@@ -0,0 +1,25 @@
+package com.vamshi;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Dataconn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{  
+			Class.forName("com.mysql.jdbc.Driver");  
+			java.sql.Connection con=DriverManager.getConnection(  
+			"jdbc:mysql://localhost:3306/karokee","root","error");  
+			//here sonoo is database name, root is username and password  
+			Statement stmt=con.createStatement();  
+			ResultSet rs=stmt.executeQuery("select * from gamer");  
+			while(rs.next())  
+			System.out.println(rs.getInt(1)+"  "+rs.getString(2));  
+			con.close();  
+			}catch(Exception e){ System.out.println(e);}  
+			}  
+	}
+
+
